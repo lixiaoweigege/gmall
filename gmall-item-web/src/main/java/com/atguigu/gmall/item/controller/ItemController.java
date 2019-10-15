50334bdb10ff6f7a3075b4a48698023cd307eaa0
2313212421 <2313212421@qq.com>
2019-10-15 14:36:24 +0800

    第一次提交，包含后台代码

--- gmall-item-web/src/main/java/com/atguigu/gmall/item/controller/ItemController.java
+++ gmall-item-web/src/main/java/com/atguigu/gmall/item/controller/ItemController.java
@@ -1,12 +1,17 @@
 package com.atguigu.gmall.item.controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ItemController {
     @RequestMapping("/{a}.html")
-    public String thymeleafTst(){
+    public String thymeleafTst(HttpServletRequest request, @PathVariable("a") String a){
+        request.setAttribute("a",a);
         return "index";
     }
 }
