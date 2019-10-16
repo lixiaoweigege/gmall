60a9c75cbef802d41b91ecdd0b6139ed2a9d8762
2313212421 <2313212421@qq.com>
2019-10-16 20:08:35 +0800

    第一次提交，包含后台代码 第二次提交，商品详情展示 第三次提交，整合redis作为商品缓存，进行性能优化

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manag/GmallManageServiceApplication.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manag/GmallManageServiceApplication.java
@@ -2,10 +2,12 @@ package com.atguigu.gmall.manag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan("com.atguigu.gmall.manag.mapper")
+@ComponentScan(basePackages = {"com.atguigu.gmall.config","com.atguigu.gmall.util"})
 public class GmallManageServiceApplication {
 
     public static void main(String[] args) {
--- gmall-manage-service/src/test/java/com/atguigu/gmall/manag/GmallManageServiceApplicationTests.java
+++ gmall-manage-service/src/test/java/com/atguigu/gmall/manag/GmallManageServiceApplicationTests.java
@@ -1,16 +1,32 @@
 package com.atguigu.gmall.manag;
 
+import com.atguigu.gmall.util.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
+import sun.awt.geom.AreaOp;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GmallManageServiceApplicationTests {
-
+@Autowired
+    RedisUtil redisUtil;
     @Test
     public void contextLoads() {
     }
+    @Test
+    public void redisTest(){
+        /*Jedis jedis=redisUtil.getJedis();
 
+        System.out.println(jedis.ping());*/
+        Jedis jedis = new Jedis("120.78.175.32", 6379);
+        String ping=jedis.ping();
+        System.out.println(ping);
+        //关闭连接
+        jedis.close();
+
+    }
 }
